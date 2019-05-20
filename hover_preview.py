41bcf0707db5128be3338440c47386c02fdc2aa8
AmjadHD <amjadhedhili@outlook.com>
2019-05-20 19:23:26 +0100

    Stop ST from reloading get_image_size.py
    - create utils folder
    - move get_image_size.py to it

--- hover_preview.py
+++ hover_preview.py
@@ -15,7 +15,7 @@ import sublime_plugin
 
 from sublime import View
 
-from .get_image_size import get_image_size, UnknownImageFormat
+from .utils.get_image_size import get_image_size, UnknownImageFormat
 
 
 TEMPLATE = """
