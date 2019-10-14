a6a1a07580dd624fd3fc16bc8f1b21cf281e8049
AmjadHD <amjadhedhili@outlook.com>
2019-10-14 14:27:06 +0100

    Fix #41

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ def on_change(s):
     image_file_re = re.compile(r"(?:"                                   # drive
                                r"\w:\\|"                                # Windows (e.g C:\)
                                r"\\\\|"                                 # Linux (\\)
-                               r"\.{0,2}[\\/]"                          # Mac OS and/or relative
+                               r"(?:\.{1,2}[\\/])?"                     # Mac OS and/or relative
                                r")"
                                r"(?:[-.@\w]+?[\\/])*"                   # body
                                r"[-.@\w]+?"                             # name
